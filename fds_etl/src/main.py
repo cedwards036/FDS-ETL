a96b38fca4f6db2851bd06c56acee50d7aa2d895
Christopher Edwards <cedwards036@gmail.com>
2020-08-14 10:26:42 -0400

    combine ugrad and masters files

--- fds_etl/src/main.py
+++ fds_etl/src/main.py
@@ -5,6 +5,8 @@ from fds_etl.src.config import CONFIG
 
 def execute():
     ugrad_df = pd.concat([pd.read_csv(f) for f in CONFIG['source_files']['undergraduate']], ignore_index=True)
+    ugrad_df['education_level'] = 'Undergraduate'
     grad_df = pd.concat([pd.read_csv(f) for f in CONFIG['source_files']['masters']], ignore_index=True)
-    print(ugrad_df.info())
-    print(grad_df.info())
+    grad_df['education_level'] = 'Masters'
+    df = pd.concat([ugrad_df, grad_df])
+    print(df.info())
