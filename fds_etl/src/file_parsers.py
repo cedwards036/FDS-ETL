643174ec19f4e2d0be1aa3939d55c07c1d4b798d
Christopher Edwards <cedwards036@gmail.com>
2020-08-14 10:41:40 -0400

    make skipping header optional when reading csv

--- fds_etl/src/file_parsers.py
+++ fds_etl/src/file_parsers.py
@@ -1,8 +1,9 @@
 import csv
 
 
-def csv_to_dict(filepath: str) -> dict:
+def csv_to_dict(filepath: str, skip_header=True) -> dict:
     with open(filepath, encoding='utf-8') as f:
-        next(f)  # skip header
+        if skip_header:
+            next(f)
         reader = csv.reader(f, skipinitialspace=True)
         return dict(reader)
